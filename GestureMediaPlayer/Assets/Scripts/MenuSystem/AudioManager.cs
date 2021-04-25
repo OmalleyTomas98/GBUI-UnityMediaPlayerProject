a3ebe7cb29a2e0ced7325c4c6c075eebad79587e
Tomas O'Malley <>
2021-04-25 15:49:33 +0100

    MediaPlayer added

---
+++ GestureMediaPlayer/Assets/Scripts/MenuSystem/AudioManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip[] musicClips;
+    private int currentTrack;
+    private AudioSource source;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            source = GetComponent<AudioSource>();
+
+            // Begin to Play Music
+            PlayMusic();
+    }
+
+
+    public void PlayMusic()
+    {
+        if(source.isPlaying){
+
+            return;
+        }
+
+        currentTrack--;
+        if(currentTrack<0){
+            currentTrack = musicClips.Length -1;
+        }
+
+        StartCoroutine(WaitForMusicEnd());
+
+    }
+    IEnumerator WaitForMusicEnd()
+    {
+        while(source.isPlaying)
+        {
+            yield return null;
+
+        }
+        NextTitle();
+
+    }
+
+    public void NextTitle()
+    {
+        source.Stop();
+        currentTrack++;
+        if (currentTrack > musicClips.Length-1)
+        {
+            currentTrack =0;
+
+        }
+        source.clip = musicClips[currentTrack];
+        source.Play();
+
+
+        // Show title
+        StartCoroutine(WaitForMusicEnd());
+    }
+}
---
+++ GestureMediaPlayer/Assets/Scripts/MenuSystem/MainMenu.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows.Speech;
+
+namespace Scripts.MenuSystem
+
+{
+public class MainMenu : MonoBehaviour
+{
+
+
+  // GrammarRecognizer and string for Voice Commands.
+        private GrammarRecognizer _grammarRecognizer;
+        private string _outAction = "";
+
+    // Enable Cursor, set time & volume and start Grammar Recognizer.
+        private void Awake()
+        {
+            // turn cursor, volume and time back on from pause menu interaction
+            Cursor.visible = true;
+            Time.timeScale = 1f;
+
+            // reset out action
+            _outAction = "";
+            // Load in grammar xml file.
+            _grammarRecognizer = new GrammarRecognizer(Path.Combine(Application.streamingAssetsPath,
+                "MainMenuControls.xml"), ConfidenceLevel.Low);
+            // Start grammar recogniser.
+            _grammarRecognizer.OnPhraseRecognized += GR_OnPhraseRecognised;
+            _grammarRecognizer.Start();
+            print("[INFO] Menu Voice Controls loaded...");
+        }
+
+
+        // Gets phases from MainMenuControls.xml and matches them to User input.
+                private void GR_OnPhraseRecognised(PhraseRecognizedEventArgs args)
+                {
+                    var message = new StringBuilder();
+                    // Read the semantic meanings from the args passed in.
+                    var meanings = args.semanticMeanings;
+                    // For each to get all the meanings.
+                    foreach (var meaning in meanings)
+                    {
+                        // Get the items for xml file.
+                        var item = meaning.values[0].Trim();
+                        message.Append("Out Action: " + item);
+                        // For calling in Update.
+                        _outAction = item;
+                    }
+
+                    // print out action detected
+                    print(message);
+                }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+          switch (_outAction)
+            {
+                // start rule
+                case "start the mediaplayer":
+                    StartMediaPlayer();
+                    break;
+                // exit rule
+                case "exit the mediaplayer":
+                    ExitMediaPlayer();
+                    break;
+            }
+        
+    }
+
+
+      // Menu buttons/commands for menu controls.
+        public void StartMediaPlayer()
+        {
+            StartCoroutine(PlayGame());
+        }
+
+        public void Controls()
+        {
+            StartCoroutine(NextScene());
+        }
+
+        public void ExitMediaPlayer()
+        {
+            Application.Quit();
+        }
+
+
+        // Fade the music & scene out and load next scene.
+        private static IEnumerator NextScene()
+        {
+            //SceneChanger.FadeToScene();
+            yield return new WaitForSeconds(1);
+            //SceneChanger.NextScene();
+        }
+
+        // Fade the music & scene out and load park scene.
+        private static IEnumerator PlayGame()
+        {
+         //   SceneChanger.FadeToScene();
+            yield return new WaitForSeconds(1);
+          //  SceneManager.LoadScene("videoPlayerScene");
+        }
+
+        // Stop the Grammar Recognizer if there is no input.
+        private void OnApplicationQuit()
+        {
+            if (_grammarRecognizer == null || !_grammarRecognizer.IsRunning) return;
+            _grammarRecognizer.OnPhraseRecognized -= GR_OnPhraseRecognised;
+            _grammarRecognizer.Stop();
+        }
+    }
+}
